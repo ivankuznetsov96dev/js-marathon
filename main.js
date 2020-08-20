0b284cd8ba562f89e88b83b1b1d69831c0fdeb9d
ivankuznetsov96dev <46788916+ivankuznetsov96dev@users.noreply.github.com>
2020-08-20 13:28:38 +0300

    Update main.js
    Second little correction to task 2

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function formattedPhone(phone) {
   if (phone.length !== 12){
     alert(`Incorrect phone number`);
   } else {
-    phone = `${phone.slice(0, 2)} (${phone.slice(2, 5)}) ${phone.slice(5, 8)}-${phone.slice(8, 10)}-${phone.slice(10, 12)}`
+    phone = `${phone.slice(0, 2)} (${phone.slice(2, 5)}) ${phone.slice(5, 8)}-${phone.slice(8, 10)}-${phone.slice(10, 12)}`;
     return phone;
   }
 }
