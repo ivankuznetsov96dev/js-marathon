4a331c948c57cdce9396cf619d6937e9eb94bbd3
KuznecKIV <ivan.kuznetsov.dev@gmail.com>
2020-08-20 13:23:34 +0300

    Second little correction to task 2

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ function formattedPhone(phone) {
   if (phone.length !== 12){
     alert(`Incorrect phone number`);
   } else {
-    phone = `${phone.slice(0, 2)} (${phone.slice(2, 5)}) ${phone.slice(5, 8)}-${phone.slice(8, 10)}-${phone.slice(10, 12)}`
+    phone = `${phone.slice(0, 2)} (${phone.slice(2, 5)}) ${phone.slice(5, 8)}-${phone.slice(8, 10)}-${phone.slice(10, 12)}`;
     return phone;
   }
 }
 
 console.log(formattedPhone(phone));
-alert(`Your number is formatted: ${formattedPhone(phone)}`)
+alert(`Your number is formatted: ${formattedPhone(phone)}`);
