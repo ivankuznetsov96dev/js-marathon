0131efc1e5a344d78b5eb91e7d6f8031c7b0fd36
KuznecKIV <ivan.kuznetsov.dev@gmail.com>
2020-08-20 13:15:20 +0300

    Task 2 completed

---
+++ main.js
@@ -0,0 +1,13 @@
+let phone = prompt("Enter your phone number:");
+
+function formattedPhone(phone) {
+  if (phone.length !== 12){
+    alert(`Incorrect phone number`);
+  } else {
+    phone = `${phone.slice(0, 2)} (${phone.slice(2, 5)}) ${phone.slice(5, 8)}-${phone.slice(8, 10)}-${phone.slice(10, 12)}`
+    return phone;
+  }
+}
+
+console.log(formattedPhone(phone));
+alert(`Your number is formatted: ${formattedPhone(phone)}`)
